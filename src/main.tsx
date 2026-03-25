037dde045b9cfec2bd14503c28bd7c5d79267654
nick_koroelv <nick.korolev2011@gmail.com>
2026-03-25 12:19:08 +0100

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import '@/index.css';
 const rootElement = document.getElementById('root');
 
 if (!rootElement) {
-  throw new Error('Root element not found.');
+  throw new Error('Root element not found');
 }
 
 createRoot(rootElement).render(
